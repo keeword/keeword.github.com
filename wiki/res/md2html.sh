84deeb0d1fb9ecbf7c15f118488708ee61f800e7
keeword <chenwenji@outlook.com>
2014-03-15 18:45:59 +0800

    矢量分析

--- wiki/res/md2html.sh
+++ wiki/res/md2html.sh
@@ -9,17 +9,18 @@ fi
 
 # pandoc选项
 PANDOC_FLAG=" --toc"                                            # 自动生成目录
-PANDOC_FLAG+=" --css=res/style.css"                                 # 指定css样式文件.
-PANDOC_FLAG+=" --template=res/pandoctpl.html"                       # pandoc模板.
+PANDOC_FLAG+=" --css=res/style.css"                             # 指定css样式文件.
+PANDOC_FLAG+=" --template=res/pandoctpl.html"                   # pandoc模板.
 PANDOC_FLAG+=" --tab-stop=4"
-PANDOC_FLAG+=" --include-before-body    res/header.html"            # 导航
-PANDOC_FLAG+=" --include-after-body     res/footer.html"            # 页脚
+PANDOC_FLAG+=" --include-before-body    res/header.html"        # 导航
+PANDOC_FLAG+=" --include-after-body     res/footer.html"        # 页脚
+PANDOC_FLAG+=" --mathjax"                                       # 不处理LaTeX语法
 
 # 预处理
-title=$(sed -ne '1s/#\(.*\)$/\1/p' $src)        # 文章标题
+title=$(sed -n -e '1s/#\(.*\)$/\1/p' $src)      # 文章标题
 date=$(date '+%Y年%m月%d日%H时')                # 文章时间
-sed -i -e '1s/#\(.*\)/<!-- \1 -->/'     $src    # 暂时删除文章标题
-
+sed -i -e '1s/\(#.*\)/<!-- \1 -->/g'    $src    # 暂时删除源文件里的文章标题
+sed -i -e 's/\[\[\([^]]*\)\]\]/\[\1\](\.\/\1\.html)/g'  $src    #处理内部链接
 PANDOC_FLAG+=" --variable title=$title"                         # 文章标题
 PANDOC_FLAG+=" --variable date=$date"                           # 更新日期
 
@@ -27,4 +28,6 @@ PANDOC_FLAG+=" --variable date=$date"                           # 更新日期
 pandoc ${PANDOC_FLAG} --from=markdown --to=html $src -o $dst    # 调用pandoc编译
 
 # 后处理
-sed -i -e '1s/<!-- \(.*\) -->/#\1/'     $src    #恢复文章标题
+sed -i -e '1s/<!-- \(.*\) -->/\1/'     $src    #恢复文章标题
+sed -i -e 's/\[\([^(]*\)\](\.\/[^[]*\.html)/\[\[\1\]\]/g'      $src    #恢复内部链接
+sed -i -e 's/<!-- #.* -->//'            $dst    #删除注释
